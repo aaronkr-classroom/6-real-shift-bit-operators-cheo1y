5fd0a531be48558f2c31d832ca2350bfa93505af
cheo1y <abyss207@naver.com>
2024-10-11 12:26:35 +0900

    Week 6

---
+++ Week 6/do-while.c
@@ -0,0 +1,36 @@
+// do-while.c
+#include <stdio.h>
+
+int main(void)
+{
+	int age;
+
+	printf("Welcome to __The Program__!\n");
+
+	do
+	{
+		printf("\nEnter your age: ");
+		scanf_s("%d", &age);
+
+		if (age >= 18)
+		{
+			printf("Access granted! ");
+			printf("Welcome to the SECRET section!\n");
+		}
+		else if (age > 0)
+		{
+			printf("Sorry, you must be 18 or order!\n");
+		}
+		else
+		{
+			printf("Invalid input. Enter a positive number.\n");
+		}
+
+	} while (age <= 0);	// 맞는 나이를 입력할 때까지
+
+	printf("Program end.\n");
+
+	getch();
+
+	return 0;
+}
---
+++ Week 6/while.c
@@ -0,0 +1,28 @@
+// while.c
+#include <stdio.h>
+
+int main(void)
+{
+	// for문 배웠을 때 sum = sum + num 했지만
+	// while문 배울 때 sum = sum * num 할게요.
+	int sum1 = 0; // 첫 시도
+	int sum2 = 1; // 두 번째 시도
+	int sum3 = 3; // 세 번째 시도
+
+	int count = 1;
+
+	while (count <= 10)
+	{
+		printf("sum(%d) * count(%d) = ", sum3, count);
+		sum3 = sum3 * count;
+		printf("%d\n", sum3);
+
+		count++;
+	}
+
+	printf("\nTotal: count = %d, sum = %d", count, sum3);
+
+	getch();
+
+	return 0;
+}
